6ee203390e13fe42962faf14267ef1b0c1b7aafe
Igor Nikolayev <igor.nikolayev@elogic.com.ua>
2022-09-13 13:05:51 +0300

    filters: price range filter customization

--- packages/venia-concept/src/components/PriceFilter/priceFilter.js
+++ packages/venia-concept/src/components/PriceFilter/priceFilter.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState} from 'react';
 import { func, shape, string } from 'prop-types';
 import MultiRangeSlider from "multi-range-slider-react";
 
+import { useLocation } from 'react-router-dom';
 import defaultClasses from './priceFilter.module.css';
 import {useStyle} from "@magento/venia-ui/lib/classify";
 import debounce from "lodash.debounce";
@@ -10,15 +11,32 @@ import setValidator from "@magento/peregrine/lib/validators/set";
 const PriceFilter = props => {
     const { filterApi, group, onApply } = props;
     const { toggleItem, removeGroup } = filterApi;
+
+    const location = useLocation();
+    const {  search } = location;
+    const urlParams = new URLSearchParams(search);
+
+    const [filterMinPrice, filterMaxPrice] = urlParams.has('price[filter]') ?
+        urlParams.get('price[filter]').split(',')[0].split('-') :
+        [null, null]
+
     const [minPrice, maxPrice] = [0, 200];
-    const [minValue, setMinValue] = useState(minPrice);
-    const [maxValue, setMaxValue] = useState(maxPrice);
+    const [minValue, setMinValue] = useState(filterMinPrice || minPrice);
+    const [maxValue, setMaxValue] = useState(filterMaxPrice || maxPrice);
     const [filterValue, setFilterValue] = useState(null)
     const classes = useStyle(defaultClasses, props.classes);
 
-    const handleInput = debounce(e => {
+    const handleRange = debounce(e => {
         setFilterValue(`${e.minValue}_${e.maxValue}`);
     }, 1000)
+
+    const handleMaxInput = debounce( (e)=> {
+        setFilterValue(`${minValue}_${e.target.value}`)
+    }, 2000);
+
+    const handleMinInput =  debounce( (e)=> {
+        setFilterValue(`${e.target.value}_${maxValue}`);
+    }, 2000);
 
     useEffect(()=> {
         if (filterValue) {
@@ -39,6 +57,17 @@ const PriceFilter = props => {
 
     return (
         <div className={classes.root}>
+            <div className={classes.inputGroup}>
+                <input type="number" value={minValue} min={0} max={200} onChange={(e) => {
+                    setMinValue(e.target.value);
+                    handleMinInput(e)
+                }}/>
+
+                <input type="number" value={maxValue} min={0} max={200} onChange={(e) => {
+                    setMaxValue(e.target.value);
+                    handleMaxInput(e)
+                }}/>
+            </div>
             <MultiRangeSlider
                 min={minPrice}
                 max={maxPrice}
@@ -51,7 +80,7 @@ const PriceFilter = props => {
                 onInput={(e) => {
                     setMinValue(e.minValue);
                     setMaxValue(e.maxValue);
-                    handleInput(e);
+                    handleRange(e);
                 }}
             />
         </div>
